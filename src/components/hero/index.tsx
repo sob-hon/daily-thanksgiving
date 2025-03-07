66cc7da487c12e822681bd4cbc5cdbc6ab718062
Sobhan Yazdanjoo <sobhan.yazdanjoo@snapppay.ir>
2025-03-07 08:19:07 +0330

    feat: add hero component

---
+++ src/components/hero/index.tsx
@@ -0,0 +1,14 @@
+import styles from "./hero.module.css";
+
+const Hero: React.FC = () => {
+  return (
+    <section className={styles.hero}>
+      <h1 className={styles.heroTitle}>صبح و شب با شکرگزاری</h1>
+      <p className={styles.heroSubtitle}>
+        .هر روز به زیبایی‌های زندگی نگاه کن و شکرگزار باشید
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
